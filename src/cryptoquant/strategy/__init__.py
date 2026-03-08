567ecc2ae02a19408b4abbc049e138babe3c30ea
bkes994104 <96167910+bkes994104@users.noreply.github.com>
2026-03-08 11:33:43 +0800

    feat(mvp5): add automated strategy optimization

--- src/cryptoquant/strategy/__init__.py
+++ src/cryptoquant/strategy/__init__.py
@@ -1,4 +1,13 @@
 from .engine import StrategyDecision, StrategyEngine
 from .ma_crossover import MovingAverageCrossoverStrategy
+from .optimizer import AutomatedStrategyOptimizer, StrategyEvaluation, StrategyOptimizationResult, StrategyParameterSet
 
-__all__ = ["StrategyDecision", "StrategyEngine", "MovingAverageCrossoverStrategy"]
+__all__ = [
+    "StrategyDecision",
+    "StrategyEngine",
+    "MovingAverageCrossoverStrategy",
+    "AutomatedStrategyOptimizer",
+    "StrategyEvaluation",
+    "StrategyParameterSet",
+    "StrategyOptimizationResult",
+]
